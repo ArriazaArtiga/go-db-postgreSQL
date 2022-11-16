9b08f496c5845931ad973dd7e2e23a5dbd8da5cf
Xalito <gonzalo.arriaza@yahoo.com>
2022-11-15 20:32:19 -0600

    Inicializando

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+
+}
+
+//Para subir nuestro proyecto a github
+/*
+Es necesario en la terminal ejecutar los siguientes comandos:
+	- git init    // para inicializar
+	- git add .   // para agregar todas las carpetas y archivos
+	- git commit  // para agregar un mensaje de los cambios
+	- Se crea el nuevo repositorio
+	- git remote add origin https://github.com/ArriazaArtiga/go-db-postgreSQL.git
+	- git branch -M main
+	- git push -u origin main
+*/
